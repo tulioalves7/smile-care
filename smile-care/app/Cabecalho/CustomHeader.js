8b6d5eac02acd220a871f72a1bcfb9c4dc7d537e
l3and49 <126525698+l3and49@users.noreply.github.com>
2024-10-07 22:47:47 -0300

    Criando cabeçalho para telas

--- smile-care/app/Cabecalho/CustomHeader.js
+++ smile-care/app/Cabecalho/CustomHeader.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import styles from './style';
-import { View, Image } from 'react-native';
+import { View, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Importa a navegação
 
 const CustomHeader = () => {
-return (
-<View style={styles.largeSemiCircle}>
-    <View style={styles.smallSemiCircle}>
-    </View>
-</View>
+  const navigation = useNavigation(); // Hook para a navegação
 
-);
+    return (
+        <View style={styles.largeSemiCircle}>
+        <View style={styles.smallSemiCircle}>
+            {/* Botão com imagem de seta */}
+            <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Home')} // Navega para a página Home
+            >
+            <Image 
+                source={require('../../assets/images/seta_esquerda.png')} 
+                style={styles.setaEsquerda} 
+            />
+            </TouchableOpacity>
+
+            {/* Logo no canto direito */}
+            <Image 
+            source={require('../../assets/images/smile_escrita.png')} 
+            style={styles.logo} 
+            />
+        </View>
+        </View>
+    );
 };
 
 export default CustomHeader;
--- smile-care/app/Cabecalho/style.ts
+++ smile-care/app/Cabecalho/style.ts
@@ -2,24 +2,38 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     largeSemiCircle: {
-        width: '130%',
-        height: '15%', // Altura maior para o semi-círculo
-        backgroundColor: '#2F3737', // Cor de fundo do semi-círculo maior
+        width: '140%',
+        height: '20%', 
+        backgroundColor: '#2F3737', 
         borderBottomLeftRadius: 200,
         borderBottomRightRadius: 200,
-        position: 'relative', // Para garantir que o semi-círculo menor fique posicionado corretamente
+        position: 'relative', 
     },
     smallSemiCircle: {
         width: '90%',
-        height: '80%',  // Ajuste para deixar um "espaço" entre os dois semi-círculos
-        backgroundColor: '#3FB3B5', // Cor do semi-círculo menor
+        height: '80%',  
+        backgroundColor: '#3FB3B5', 
         borderBottomLeftRadius: 200,
         borderBottomRightRadius: 200,
         position: 'absolute',
-        left: '5%', // Centralizar horizontalmente
-        justifyContent: 'center', // Centralizar o conteúdo verticalmente
-        alignItems: 'center', // Centralizar o conteúdo horizontalmente
+        left: '5%', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
     },
+    logo: {
+        width: 155,
+        height: 30,
+        position: 'absolute', 
+        top: 20, // Mantém no topo
+        right: 20, // Alinha à direita
+    },
+    setaEsquerda: {
+        width: 30, // Ajuste o tamanho da seta conforme necessário
+        height: 30,
+        position: 'absolute', 
+        top: 20, // Mantém no topo
+        left: 20, // Alinha à esquerda
+    }
 });
 
 export default styles;
